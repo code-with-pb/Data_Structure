b5108897eb6e7aac42ebea9a3819e6df54d4b946
code-with-pb <codexpythonpb@gmail.com>
2023-12-22 01:42:10 +0530

    Reverse Stack

---
+++ Stack/St32.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class St32 {
+	static class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data = data;
+			next = null;
+		}
+	}
+	static class Stack{
+		public static Node head;
+		public static boolean isEmpty() {
+			return head == null;
+		}
+		public static void push(int data) {
+			Node nd = new Node(data);
+			if(isEmpty()) {
+				head = nd;
+				return;
+			}
+			nd.next = head;
+			head = nd;
+		}
+		public static int pop() {
+			if(isEmpty()) {
+				return -1;
+			}
+			int t = head.data;
+			head = head.next;
+			return t;
+		}
+		public static int peek() {
+			if(isEmpty()) {
+				return -1;
+			}
+			return head.data;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		St32.Stack s = new St32.Stack();
+		
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		
+		while(!s.isEmpty()) {
+			System.out.println(s.peek());
+			s.pop();
+		}
+	}
+
+}
