2f9d9c1ad68b22073ec96a1aa59f19ad21dba33c
code-with-pb <codexpythonpb@gmail.com>
2023-12-22 01:41:24 +0530

    Stack using ArrayList

---
+++ Stack_ArrayList/SA4.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class SA4 {
+	static class Stack{
+		static ArrayList<Integer> list = new ArrayList<>();
+		public static boolean isEmpty() {
+			return list.size()==0;
+		}
+		public static void push(int data) {
+			list.add(data);
+		}
+		public static int pop() {
+			if(isEmpty()) {
+				return -1;
+			}
+			int t = list.get(list.size()-1);
+			list.remove(list.size()-1);
+			return t;
+		}
+		public static int peek() {
+			if(isEmpty()) {
+				return -1;
+			}
+			return list.get(list.size()-1);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack s = new Stack();
+		
+		s.push(10);
+		s.pop();
+		s.push(30);
+		s.push(20);
+		s.push(10);
+		
+		while(!s.isEmpty()) {
+			System.out.println(s.peek());
+			s.pop();
+		}
+	}
+
+}
