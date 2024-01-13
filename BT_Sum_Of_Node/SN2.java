048f3e775241598bddf1958a7fe0bb5e89f6fccc
code-with-pb <codexpythonpb@gmail.com>
2024-01-13 12:52:18 +0530

    BT Sum Of Node

---
+++ BT_Sum_Of_Node/SN2.java
@@ -0,0 +1,49 @@
+
+public class SN2 {
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data){
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	static class Tree{
+		static int idx = -1;
+		public static Node buildTree(int nodes[]) {
+			idx++;
+			
+			if(nodes[idx] == -1) {
+				return null;
+			}
+			Node nd = new Node(nodes[idx]);
+			nd.left = buildTree(nodes);
+			nd.right = buildTree(nodes);
+			
+			return nd;
+			
+		}
+	}
+	public static int SN(Node root) {
+		if(root == null) {
+			return 0;
+		}
+		int leftNode = SN(root.left);
+		int rightNode = SN(root.right);
+		
+		return leftNode + rightNode + root.data;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nodes[] = {7,2,1,-1,-1,3,-1,-1,4,5,-1,-1,6,-1,-1};
+		Tree t = new Tree();
+		Node root = t.buildTree(nodes);
+		
+		
+		System.out.println(SN(root));
+	}
+
+}
