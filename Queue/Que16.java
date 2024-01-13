0da9087a97bfc70121dc13e09734eb483cc4308a
code-with-pb <codexpythonpb@gmail.com>
2024-01-13 12:42:52 +0530

    Queue

---
+++ Queue/Que16.java
@@ -0,0 +1,52 @@
+
+public class Que16 {
+	static class Queue{
+		static int a[];
+		static int s;
+		static int r = -1;
+		
+		Queue(int n){
+			a = new int[n];
+			this.s = n;
+		}
+		public static boolean isEmpty() {
+			return r == -1;
+		}
+		public static void add(int data) {
+			if(r == s - 1) {
+				return;
+			}
+			r++;
+			a[r] = data;
+		}
+		public static int pop() {
+			if(isEmpty()) {
+				return -1;
+			}
+			int t = a[0];
+			for(int i=0;i<r;i++) {
+				a[i] = a[i+1];
+			}
+			r--;
+			
+			return t;
+		}
+		public static int peek() {
+			if(isEmpty()) {
+				return -1;
+			}
+			return a[0];
+		}
+	}
+	public static void main(String args[]) {
+		Queue q = new Queue(5);
+		q.add(10);
+		q.add(20);
+		q.add(30);
+		
+		while(!q.isEmpty()) {
+			System.out.print(q.peek()+" ");
+			q.pop();
+		}
+	}
+}
