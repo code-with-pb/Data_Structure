d615d84b24e457f174a4b2b07963268a6e132da7
code-with-pb <codexpythonpb@gmail.com>
2024-02-14 14:09:55 +0530

    Circular Queue

---
+++ Circular_Queue/CQ19.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class CQ19 {
+	static class Queue{
+		static int a[];
+		static int s;
+		static int f = -1,r = -1;
+		
+		Queue(int n){
+			a = new int[n];
+			this.s = n;
+		}
+		public static boolean isEmpty() {
+			return f == -1 && r == -1; 
+		}
+		public static boolean isFull() {
+			return (r+1)%s == f;
+		}
+		public static void add(int d) {
+			if(isFull()) {
+				return;
+			}
+			if(f == -1) {
+				f = 0;
+			}
+			r = (r+1)%s;
+			a[r] = d;
+		}
+		public static int remove() {
+			if(isEmpty()) {
+				return -1;
+			}
+			int t = a[f];
+			if(f == r) {
+				f = r = -1;
+			}
+			else {
+				f = (f+1)%s;
+			}
+			return t;
+		}
+		public static int peek() {
+			if(isEmpty()) {
+				return -1;
+			}
+			return a[f];
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue q = new Queue(5);
+		
+		q.add(10);
+		q.add(20);
+		q.add(30);
+		q.add(40);
+		q.remove();
+		q.add(50);
+		
+		while(!q.isEmpty()) {
+			System.out.print(q.peek()+" ");
+			q.remove();
+		}
+	}
+
+}
