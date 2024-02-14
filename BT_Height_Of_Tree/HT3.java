37f8b05a7ae06e48bf10408f4d33392dd01d0f89
code-with-pb <codexpythonpb@gmail.com>
2024-02-14 14:13:20 +0530

    Bt Height of Tree

---
+++ BT_Height_Of_Tree/HT3.java
@@ -0,0 +1,59 @@
+
+
+public class HT3 {
+	static class Node{
+		int data;
+		Node l;
+		Node r;
+		
+		Node(int data){
+			this.data = data;
+			this.l = null;
+			this.r = null;
+		}
+	}
+	/*static class Tree{
+	 * 	int i=-1;
+	 * 	public static Node Bt(int a[]){
+	 * 		i++;
+	 * 		if(a[i] == -1){
+	 * 			return null;
+	 * 		}
+	 * 		Node nd = new Node(a[i]);
+	 * 		nd.l = Bt(a);
+	 * 		nd.r = Bt(a);
+	 * 		return nd;
+	 * 	}
+	 * }*/
+	public static Node insert(Node root, int val) {
+		if(root == null) {
+			root = new Node(val);
+			return root;
+		}
+		if(root.data > val) {
+			root.l = insert(root.l,val);
+		}
+		else {
+			root.r = insert(root.r,val);
+		}
+		return root;
+	}
+	public static int Hn(Node root) {
+		if(root == null) {
+			return 0;
+		}
+		int ln = Hn(root.l);
+		int rn = Hn(root.r);
+		
+		return Math.max(ln, rn)+ 1;
+	}
+	public static void main(String args[]) {
+		int a[] = {9,8,7,3,2,1,4,5,6};
+		Node root = null;
+		
+		for(int i=0;i<a.length;i++) {
+			root = insert(root, a[i]);
+		}
+		System.out.print(Hn(root));
+	}
+}
